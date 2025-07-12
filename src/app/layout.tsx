99b34a33855e889bdea0400c4411588b04d7c7c4
leeyeonwoong <(uc2412030@gmail.com)>
2025-07-12 22:25:27 +0900

    드디어 아마 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
+
+
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const inter = Inter({
+  subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
@@ -24,12 +21,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${inter.className} antialiased`}
         >
           {children}
         </body>
